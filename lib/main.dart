16f10f253f538171bff440e9840541d61d2b8e86
Pankaj Jangid <pankaj0619@gmail.com>
2023-05-08 19:39:26 +0530

    riverpod using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nameProvider = Provider<String>((ref) {
+  return "hello Pankaj";
+});
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Riverpod Demo",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Home(),
+    );
   }
 }
+// using stateful widget
+
+class Home extends ConsumerStatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<Home> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
+  @override
+  Widget build(BuildContext context) {
+    final name = ref.watch(nameProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("River pod Provider Demo"),
+      ),
+      body: Center(
+        child: Text(name),
+      ),
+    );
+  }
+}
+
+
+// using Consumer
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("River pod Provider Demo"),
+      ),
+      body: Center(
+        child: Consumer(builder: (context, ref, child) {
+          final name = ref.watch(nameProvider);
+          return Text(name);
+        }),
+      ),
+    );
+  }
+}
+/*
+
+// using ConsumerWidget 
+
+class MyHome extends ConsumerWidget{
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+
+    String name = ref.watch(nameProvider);
+    return Scaffold(
+
+      appBar: AppBar(title: Text(name),),
+      body: Center(),
+    );
+  }
+}
+*/
