fb10e1840971f945e1ca2cb222550a6b8ea880e6
Pankaj Jangid <pankaj0619@gmail.com>
2023-05-08 19:55:06 +0530

    riverpod using state provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nameProvider = Provider<String>((ref) {
-  return "hello Pankaj";
+var countProvider = StateProvider<int>((ref){
+  return 0;
 });
 
 void main() {
@@ -33,54 +35,34 @@ class Home extends ConsumerStatefulWidget {
 class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
-    final name = ref.watch(nameProvider);
+    var count = ref.watch(countProvider);
+
+    ref.listen(countProvider, (previous, next) {
+      if(next ==5){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("The counter value is $next")));
+      }
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text("River pod Provider Demo"),
+        actions: [
+          IconButton(onPressed: (){
+         //   ref.invalidate(countProvider);
+            ref.refresh(countProvider);
+          }, icon: Icon(Icons.refresh))
+        ],
       ),
       body: Center(
-        child: Text(name),
+        child: Text("$count",style: TextStyle(fontSize: 30),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+         // ref.read(countProvider.notifier).update((state) => state+1);
+          ref.read(countProvider.notifier).state ++;
+        },
+        child: Icon(Icons.add),
       ),
     );
+
   }
 }
-
-
-// using Consumer
-class MyHome extends StatelessWidget {
-  const MyHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("River pod Provider Demo"),
-      ),
-      body: Center(
-        child: Consumer(builder: (context, ref, child) {
-          final name = ref.watch(nameProvider);
-          return Text(name);
-        }),
-      ),
-    );
-  }
-}
-/*
-
-// using ConsumerWidget 
-
-class MyHome extends ConsumerWidget{
-  const MyHome({super.key});
-
-  @override
-  Widget build(BuildContext context,WidgetRef ref) {
-
-    String name = ref.watch(nameProvider);
-    return Scaffold(
-
-      appBar: AppBar(title: Text(name),),
-      body: Center(),
-    );
-  }
-}
-*/
